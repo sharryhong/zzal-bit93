e87250b60da74582bb68d7abeab42820fef48247
sharryhong <kshopzoa15@gmail.com>
2017-07-12 16:47:08 +0900

    링크

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -12,26 +12,24 @@
     let zzalList1 = $('#zzal-list1');
     let zzalList2 = $('#zzal-list2');
     let zzalWeekList= $('#zzal-week-list');
+    var templateFn = null;
+    var generatedHTML = null;
     
     $.getJSON('mainList.json', function(result) {
 	  // 템플릿을 실행하는 함수 리턴
-	  let templateFn1 = Handlebars.compile($('#main-template1').text())
-	  let generatedHTML1 = templateFn1(result.data)
-	  zzalList1.text('')
-	  zzalList1.html(generatedHTML1)
-	    
-	  let templateFn2 = Handlebars.compile($('#main-template2').text())
-	  let generatedHTML2 = templateFn2(result.data)
-	  zzalList2.text('')
-	  zzalList2.html(generatedHTML2)
+      generateHandlebars(result, $('#main-template1'), zzalList1);
+      generateHandlebars(result, $('#main-template2'), zzalList2);
 	})
 	  
     $.getJSON('mainWeekList.json', function(result) {
-      console.log(result.data.weekList)
-      let templateFn = Handlebars.compile($('#main-week-template').text())
-	  let generatedHTML = templateFn(result.data)
-	  zzalWeekList.text('')
-	  zzalWeekList.html(generatedHTML)
+      generateHandlebars(result, $('#main-week-template'), zzalWeekList);
     })
+    
+    function generateHandlebars(result, el, target) {
+      templateFn = Handlebars.compile(el.text())
+  	  generatedHTML = templateFn(result.data)
+  	  target.text('')
+  	  target.html(generatedHTML)
+    }
     
 })(jQuery);
