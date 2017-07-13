7bcf8b2923aadf6270732f72e1883d2d0c6bacc0
sharryhong <kshopzoa15@gmail.com>
2017-07-13 12:22:12 +0900

    tabmenu 작업중

--- src/main/webapp/js/mypage.js
+++ src/main/webapp/js/mypage.js
@@ -1,10 +1,38 @@
 (function($){
   'use strict';
 
-  tabMenu($('.select_info .menu'))
-  tabMenu($('.zzal-menu .menu'), $('.zzal-con'))
-  var index = null
+ /* let $zzalMenu = $('.zzal-menu')
+  let $zzalWrap = $('.zzal-wrap')
+  let $zzalCon = $('.zzal-con')
+  let index = null
   
+  $zzalMenu.css("display", "none")
+  $zzalMenu.eq(0).css("display", "block")
+  $zzalWrap.css("display", "none")
+  $zzalWrap.eq(0).css("display", "block")
+  
+  tabMenu($('.select_info .menu'), $zzalMenu)
+  tabMenu($('.zzal-menu .menu'), $zzalWrap)
+  
+  // 매개변수 : 탭메뉴들, 메뉴 누르면 나오는 컨텐츠들
+  function tabMenu(els, con) {
+	els.click(function() {
+	  els.removeClass('on')
+	  $(this).addClass('on')
+	  index = $(this).index()
+	  con.css("display", "none")
+	  con.eq(index).css("display", "block")
+	})
+  }*/
+  
+  let index = null
+  let $zzalWrap = $('.zzal-wrap')
+  tabMenu($('.select_info .menu'), $zzalWrap)
+  
+  $zzalWrap.css("display", "none")
+  $zzalWrap.eq(0).css("display", "block")
+  
+  //매개변수 : 탭메뉴들, 메뉴 누르면 나오는 컨텐츠들
   function tabMenu(els, con) {
 	els.click(function() {
 	  els.removeClass('on')
