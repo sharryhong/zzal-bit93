10d99af00ae5f738b73dc4f26c9778ed6c8a1ec2
sharryhong <kshopzoa15@gmail.com>
2017-07-13 15:00:06 +0900

    tabmenu

--- src/main/webapp/js/mypage.js
+++ src/main/webapp/js/mypage.js
@@ -1,36 +1,23 @@
 (function($){
   'use strict';
-
- /* let $zzalMenu = $('.zzal-menu')
-  let $zzalWrap = $('.zzal-wrap')
-  let $zzalCon = $('.zzal-con')
-  let index = null
-  
-  $zzalMenu.css("display", "none")
-  $zzalMenu.eq(0).css("display", "block")
-  $zzalWrap.css("display", "none")
-  $zzalWrap.eq(0).css("display", "block")
-  
-  tabMenu($('.select_info .menu'), $zzalMenu)
-  tabMenu($('.zzal-menu .menu'), $zzalWrap)
-  
-  // 매개변수 : 탭메뉴들, 메뉴 누르면 나오는 컨텐츠들
-  function tabMenu(els, con) {
-	els.click(function() {
-	  els.removeClass('on')
-	  $(this).addClass('on')
-	  index = $(this).index()
-	  con.css("display", "none")
-	  con.eq(index).css("display", "block")
-	})
-  }*/
-  
+ 
   let index = null
   let $zzalWrap = $('.zzal-wrap')
+  let $zzalCon01 = $('.zzal-con01')
+  let $zzalCon02 = $('.zzal-con02')
+  
   tabMenu($('.select_info .menu'), $zzalWrap)
+  tabMenu($('.zzal-menu01 .menu'), $zzalCon01)
+  tabMenu($('.zzal-menu02 .menu'), $zzalCon02)
   
-  $zzalWrap.css("display", "none")
-  $zzalWrap.eq(0).css("display", "block")
+  setTab($zzalWrap)
+  setTab($zzalCon01)
+  setTab($zzalCon02)
+  
+  function setTab(els) {
+	els.css("display", "none")
+    els.eq(0).css("display", "block")
+  }
   
   //매개변수 : 탭메뉴들, 메뉴 누르면 나오는 컨텐츠들
   function tabMenu(els, con) {
