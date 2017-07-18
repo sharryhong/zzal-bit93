817ef23fb141b428cf1e334249f2f5e30ecce2af
sharryhong <kshopzoa15@gmail.com>
2017-07-18 12:21:51 +0900

    .

--- src/main/webapp/js/load.js
+++ src/main/webapp/js/load.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){ 
 	$.get("header.html", function(data) {
 	  $("#header").html(data);
+	  $.getScript("./scrollbar/jquery.mCustomScrollbar.concat.min.js");
 	  $.getScript("./js/slide.js");
 	  $.getScript("./js/main.js");
 	  $.getScript("./js/search.js");
