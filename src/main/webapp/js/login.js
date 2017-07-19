058a04c74f5072924f913d91f4454e33d94de72b
kdy8982 <kdy8982@naver.com>
2017-07-19 11:56:44 +0900

    .
    .

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,11 +1,17 @@
 (function($) {
 	$('.login').click(function() {
 		console.log('lolol')
-		
+
 		$(".login-curtain").show();
 		$(".container").show();
-		
-	$("body").css("overflow", "hidden");
+		$("body").css("overflow", "hidden");
+			
 	});
+	
+	$('.close-btn').click(function() {
+		$(".login-curtain").show();
+
+	});
+	
 
 })(jQuery);
