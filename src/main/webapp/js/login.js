7476c970ddf4f2ae62cd01856a06dfebcc4dff92
kdy8982 <kdy8982@naver.com>
2017-07-19 14:00:26 +0900

    회원가입, 로그인 완료

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -12,8 +12,19 @@
 		console.log('closebtn click!!!')
 		$(".login-curtain").hide();
 		$(".container").hide();
+		$(".signup-container").hide();
 	    $("body").css("overflow", "visible");
+	});
+	
+	$('.sign-in').click(function() {
+		console.log('sign-in click!!!')
+		$(".container").hide();
+		$(".signup-container").show();
+		$(".logo").hide();
+		$(".mini-logo").hide();
 
+
+		$("body").css("overflow", "hidden");
 
 	});
 	
