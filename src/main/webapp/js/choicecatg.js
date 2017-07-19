70eadf71161c22ed8b46610bbbf184091c20698e
정성룡 <fyd519@gmail.com>
2017-07-19 14:01:15 +0900

    카테고리 사진 및

--- src/main/webapp/js/choicecatg.js
+++ src/main/webapp/js/choicecatg.js
@@ -1,21 +1,28 @@
 (function($){
 	'use strict';
+//
+ let	choicecategorys1 = $('#choice-categorys');
+//
+// $.getJSON('choicecatg.json',function(result){
+//   console.log(result)
+//
+//   let templateFn1 = Handlebars.compile($('#choiceCategory-template1').text())
+//   let generatedHTML1 = templateFn1(result.data)
+//   console.log(generatedHTML1)
+//   choicecategorys1.text('')
+//   choicecategorys1.html(generatedHTML1)
 
-let	choicecategorys1 = $('#choice-categorys');
+// });
 
+	$('.choice-btn').click(function() {
+		console.log('choice')
+		$('input:checkbox[id="music-catg"]').attr("checked", true);
+		console.log('checked')
+		$('input:checkbox[id="music-catg"]').val();
+		console.log("값 보내기")
 
-$.getJSON('choicecatg.json',function(result){
-  console.log(result)
+		// $(".chooosecatg").val();
 
-  let templateFn1 = Handlebars.compile($('#choiceCategory-template1').text())
-  let generatedHTML1 = templateFn1(result.data)
-  console.log(generatedHTML1)
-  choicecategorys1.text('')
-  choicecategorys1.html(generatedHTML1)
-	
-	})
-
-
-
+	});
 
 })(jQuery);
