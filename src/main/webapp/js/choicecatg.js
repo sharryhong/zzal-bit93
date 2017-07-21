051f0e27bb84eb35dd2421714878581fec9ccfb0
정성룡 <fyd519@gmail.com>
2017-07-21 21:08:56 +0900

    .

--- src/main/webapp/js/choicecatg.js
+++ src/main/webapp/js/choicecatg.js
@@ -1,25 +1,21 @@
 (function($){
 	'use strict';
-//
- let	choicecategorys1 = $('#choice-categorys');
-//
-// $.getJSON('choicecatg.json',function(result){
-//   console.log(result)
-//
-//   let templateFn1 = Handlebars.compile($('#choiceCategory-template1').text())
-//   let generatedHTML1 = templateFn1(result.data)
-//   console.log(generatedHTML1)
-//   choicecategorys1.text('')
-//   choicecategorys1.html(generatedHTML1)
 
-// });
+	$.getJSON('category/list.json',function(data){
+		console.log(data)
+	})
+
 
 	$('.choice-btn').click(function() {
 		console.log('choice')
-		$('input:checkbox[clss="music-catg"]').val();
-		console.log("값 보내기")
 
-		// $(".chooosecatg").val();
+
+		$('.click-checkbox:checked').each(function() {
+      	($(this).val());
+				console.log(this)
+
+   });
+
 
 	});
 
