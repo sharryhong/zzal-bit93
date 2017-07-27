6c25de0d04d64d45fc680105544f1bb6f09bec0d
SinYongJu <ygp007@naver.com>
2017-07-27 17:07:47 +0900

    zxzxzxzx

--- src/main/webapp/js/detailpage.js
+++ src/main/webapp/js/detailpage.js
@@ -70,6 +70,31 @@ $('#like-btn').on('click',function(event){
 
 })
 
+
+$('#m-like-btn').on('click',function(event){
+  console.log('좋아요 안이야! 114')
+  
+//  leftBtnFunc(this, islike);
+  
+  let up = $(this)[0].children[0]
+  let off = $(this)[0].children[1]
+ if(!islike){
+   console.log(islike)
+   $(up).removeClass('off-btn')
+   $(off).addClass('off-btn')
+   return islike=true;
+ }else{
+   console.log(islike)
+   $(off).removeClass('off-btn')
+   $(up).addClass('off-btn')
+   return islike=false;
+ }
+
+})
+
+
+
+
 var isvolumeOn = false;
 
 console.log('재생 안이야! 117')
@@ -93,6 +118,11 @@ $('#volume-btn').on('click',function(event){
 
 var isPlay = false
 
+
+
+
+
+
 $('#play-btn').on('click',function(event){
 //	leftBtnFunc(isPlay)
    let up = $(this)[0].children[0]
@@ -111,6 +141,26 @@ $('#play-btn').on('click',function(event){
 })
 
 
+$('#m-play-btn').on('click',function(event){
+//	leftBtnFunc(isPlay)
+   let up = $(this)[0].children[0]
+   let off = $(this)[0].children[1]
+  if(!isPlay){
+    console.log(isPlay)
+    $(up).removeClass('off-btn')
+    $(off).addClass('off-btn')
+    return isPlay=true;
+  }else{
+    console.log(isPlay)
+    $(off).removeClass('off-btn')
+    $(up).addClass('off-btn')
+    return isPlay=false;
+  }
+})
+
+
+
+
 
 $('.detail-funcbtn-a').on('click', function(e){
     let layer =$(this).attr('href')
@@ -118,12 +168,6 @@ $('.detail-funcbtn-a').on('click', function(e){
 })
 
 
-//
-// var lstPagenOm = $('.detail-page-swipe .det-lect'),
-//     lstPagenUm= ($('.detail-page-swipe .det-lect').length)-1;
-
-// console.log($(lstPagenOm[lstPagenUm]))
-//var testnum =$('.swiper-wrapper.detail-page-swipe .swiper-pagination.main-disabled.swiper-pagination-fraction')
 
 var zzno=1
 $.getJSON('zzal/list.json',{'zzno': zzno},function(result){
@@ -132,20 +176,5 @@ $.getJSON('zzal/list.json',{'zzno': zzno},function(result){
   let str = usedata.cdt
   let res = str.split(" "); 
   console.log(res[0])
-//  console.log(typeof(str))
-//  let templateFn = Handlebars.compile($('#detailright-user-template').text())
-//  let generatedHTML = templateFn(result.data) // 템플릿 함수에 데이터를 넣고 HTML을 생성한다.
-//  console.log(generatedHTML)
-//  $('#detailright-info').text('') // tbody의 기존 tr 태그들을 지우고
-//  $('#detailright-info').html(generatedHTML) // 새 tr 태그들로 설정한다.
-  //
-  // templateFn = Handlebars.compile($('#mypagemid-template').text())
-  // generatedHTML = templateFn(result.data)
-  // $('#mypage-mid').text('')
-  // $('#mypage-mid').html(generatedHTML)
-  //
-  // templateFn = Handlebars.compile($('#mypagebot-template').text())
-  // generatedHTML = templateFn(result.data)
-  // $('#mypage-bot').text('')
-  // $('#mypage-bot').html(generatedHTML)
+
 })
