8e28eeb9cb7db0319ec6881b20800d8c3ef85e59
kdy8982 <kdy8982@naver.com>
2017-07-31 10:18:44 +0900

    .
    .

--- database/datakdy.sql
+++ database/datakdy.sql
@@ -21,3 +21,18 @@ insert into zzal_lect(zzno,mno,cno,titl,cdt) values(1,1,1,'짤강1','2017-01-31'
 
 -- 존재하는 칼럼에 추가해넣기 
 update memb set pic='sul.jpg' where mno='2';
+
+
+
+-- 새로운 짤 강의 추가(insert)
+insert into zzal_lect (zzno, mno, cno, titl, pic, cdt) values(100, 1, 1, '댓글달기강의', 'sul.jpg', '2017-07-26');
+insert into zzal_lect (zzno, mno, cno, titl, pic, cdt) values(200, 2, 2, '사진 잘 찍기 강의', 'sul.jpg', '2017-05-26');
+
+-- 새로운 댓글 추가(insert)
+insert into reply(cont, cdt, re_like,report,zzno,mno) values(3,'나는 3댓글 내용입니다 ', curdate(), true, false, 100, 1);
+insert into reply(cont, cdt, re_like,report,zzno,mno) values(4,'댓글4입니다. 정말 이쁜 사진이네요!', curdate(), true, false, 200, 1);
+
+
+-- 댓글 테이블 검색
+select rno, cont, date_format(cdt, '%Y-%m-%d %H:%i'), re_like, report, zzno, mno
+from reply
