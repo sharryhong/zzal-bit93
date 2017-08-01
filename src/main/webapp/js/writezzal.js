df349555b590985a2f7060ca93f233e7444c42d4
SinYongJu <ygp007@naver.com>
2017-08-01 16:40:16 +0900

    킈ㅡ키

--- src/main/webapp/js/writezzal.js
+++ src/main/webapp/js/writezzal.js
@@ -4,8 +4,8 @@
 	    fiTitle = $('#fi-title'),
 	    fiCollect = $('#select-collect'),
 	    fiCategory = $('#select-category')
-	
-	var no	
+
+	var no
 	$.getJSON('/zzal-bit93/auth/userinfo.json', function(result) {
 	  if (result.data) {
 		  no = result.data.no
@@ -15,14 +15,14 @@
 		  getCollect(no)
 	  }
 	})
-	
-	// 핸들바스. main.js에 있는 함수 사용 
-	// 카테고리 리스트 뿌려주기 
+
+	// 핸들바스. main.js에 있는 함수 사용
+	// 카테고리 리스트 뿌려주기
 	$.getJSON('/zzal-bit93/category/list.json', function(result) {
         generateHandlebars(result, $('#select-category-template'), $('#select-category'));
     })
-    
-    // 컬렉션 리스트 뿌리기 
+
+    // 컬렉션 리스트 뿌리기
     function getCollect(no) {
 		$.getJSON('/zzal-bit93/collect/list.json', {'no': no}, function(result) {
 			if (result.data) {
@@ -30,7 +30,22 @@
 			}
 		})
 	}
-	
+
+
+	$('.swiper-button-prev').on('click',function(e){
+		console.log(swiper)
+
+		// console.log($('.swiper-pagination-current')[0].innerHTML)
+	})
+
+	$('.swiper-button-next').on('click',function(e){
+		console.log(swiper)
+
+		// console.log($('.swiper-pagination-current')[0].innerHTML)
+	})
+
+
+
   $('#fi-photoupload').fileupload({
 	    url: '/zzal-bit93/zzal/upload.json',
 	    dataType: 'json',
@@ -38,9 +53,11 @@
 	    disableImageResize: /Android(?!.)|Opera/
 	        .test(window.navigator && navigator.userAgent),
 		previewMaxWidth: 670,   // 미리보기 이미지 너비
-		previewMaxHeight: 500,  // 미리보기 이미지 높이 
+		previewMaxHeight: 500,  // 미리보기 이미지 높이
 		previewCrop: true,      // 미리보기 이미지를 출력할 때 원본에서 지정된 크기로 자르기
 		processalways: function(e, data) {
+
+
 		      var imagesDiv = $('#images-div');
 		      imagesDiv.html("");
 		      for (var i = 0; i < data.files.length; i++) {
@@ -51,11 +68,11 @@
 		        } catch (err) {}
 		      }
 		},
-	    done: function (e, data) { 
+	    done: function (e, data) {
 	      console.log('done()...');
-	      console.log(data.result); 
+	      console.log(data.result);
 	      var filenames = data.result.data;
-	      
+
 	      for (var i = 0; i < filenames.length; i++) {
 	        var val = fiFilenames.val();
 	        if (val.length > 0) val += ",";
@@ -63,7 +80,7 @@
 	      }
 	    }
 	});
-  
+
     $(document).on('click', '#add-btn, #temp-save-btn', function() {
     	console.log(fiFilenames.val())
     	$.post('/zzal-bit93/zzal/add.json', {
@@ -77,15 +94,24 @@
     		/* location.href = 'index.html'*/
     	}, 'json')
     })
-    
-    var swiper = new Swiper('.swiper-container', {
-    	nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        pagination: '.swiper-pagination',
-        paginationType: 'fraction',
-        paginationClickable: true
-    });
-    
+
+		var swiper = new Swiper('.swiper-container', {
+			nextButton: '.swiper-button-next',
+				prevButton: '.swiper-button-prev',
+				pagination: '.swiper-pagination',
+				paginationType: 'fraction',
+				paginationClickable: true
+		});
+		
+
+
+
+		$(document).on('ready', function(e){
+
+			console.log(swiper.realIndex)
+			console.log($('.writezzal-page.swiper-slide-active').attr('data-no'))
+		})
+
     $(document).on('click', '.append-slide', function(e) {
     	e.preventDefault();
     	templateFn = Handlebars.compile($('#addpage-template').text())
