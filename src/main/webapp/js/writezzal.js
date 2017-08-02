4a4e174584245100272326b27cbd8c383748dce0
SinYongJu <ygp007@naver.com>
2017-08-02 10:18:11 +0900

    ㅋㅋ

--- src/main/webapp/js/writezzal.js
+++ src/main/webapp/js/writezzal.js
@@ -1,6 +1,6 @@
 (function($){
 	'use strict';
-	var fiFilenames = $('#fi-filenames'),
+	var fiFilenames = $('.fi-filenames'),
 	    fiTitle = $('#fi-title'),
 	    fiCollect = $('#select-collect'),
 	    fiCategory = $('#select-category')
@@ -32,47 +32,75 @@
 	}
 
 
-	$('.swiper-button-prev').on('click',function(e){
-		console.log(swiper)
-
-		// console.log($('.swiper-pagination-current')[0].innerHTML)
-	})
-
-	$('.swiper-button-next').on('click',function(e){
-		console.log(swiper)
-
-		// console.log($('.swiper-pagination-current')[0].innerHTML)
-	})
 
 
 
-  $('#fi-photoupload').fileupload({
+	//
+	// $('.swiper-button-prev').on('click',function(e){
+	// 	console.log(indexNum, 'indexNum임 프리')
+	// 	setTimeout(clickDone, 150);
+	// 	// console.log($('.swiper-pagination-current')[0].innerHTML)
+	// })
+	//
+	// $('.swiper-button-next').on('click',function(e){
+	// 	console.log(indexNum, 'indexNum임 넥스')
+	// 	setTimeout(clickDone,  150);
+	// 	// console.log($('.swiper-pagination-current')[0].innerHTML)
+	// })
+
+var indexNum=0;
+
+	function clickDone(){
+    console.log($('.images-div'))
+		indexNum = parseInt(swiper.realIndex);
+		return indexNum;
+	}
+
+var imagesDi;
+
+
+  $('.photo-upload-btn').fileupload({
 	    url: '/zzal-bit93/zzal/upload.json',
 	    dataType: 'json',
-	    imageMaxWidth: 1000,
+	    imageMaxWidth: 670,
 	    disableImageResize: /Android(?!.)|Opera/
 	        .test(window.navigator && navigator.userAgent),
-		previewMaxWidth: 670,   // 미리보기 이미지 너비
-		previewMaxHeight: 500,  // 미리보기 이미지 높이
-		previewCrop: true,      // 미리보기 이미지를 출력할 때 원본에서 지정된 크기로 자르기
-		processalways: function(e, data) {
-
-
-		      var imagesDiv = $('#images-div');
-		      imagesDiv.html("");
+			previewMaxWidth: 670,   // 미리보기 이미지 너비
+			previewMaxHeight: 720,  // 미리보기 이미지 높이
+			previewCrop: true,      // 미리보기 이미지를 출력할 때 원본에서 지정된 크기로 자르기
+			processalways: function(e, data) {
+					// var inNum = parseInt(indexNum);
+					// console.log(filenames,'processalways안이얌')
+					imagesDiv = $('.images-div')[indexNum];
+					console.log(imagesDiv,indexNum)
 		      for (var i = 0; i < data.files.length; i++) {
 		        try {
-		          if (data.files[i].preview.toDataURL) {
-		        	  imagesDiv.css("background-image", 'url(' + data.files[i].preview.toDataURL() +')');
-		          }
+							console.log($(imagesDiv))
+
+								if (data.files[i].preview.toDataURL) {
+											$(imagesDiv).html("");
+
+
+										if(indexNum>0){
+													console.log(imagesDiv,indexNum)
+													console.log(data.files[i])
+													$('<img>').attr('src',data.files[i].preview.toDataURL())
+																		.css({"max-width" : '670',
+																					"max-height" :"720"})
+																		.appendTo($(imagesDiv))
+										}else{
+							            $(imagesDiv).css("background-image", 'url(' + data.files[i].preview.toDataURL() +')');
+										}
+								}//if data
+
 		        } catch (err) {}
 		      }
 		},
 	    done: function (e, data) {
 	      console.log('done()...');
-	      console.log(data.result);
+	      // console.log(data.result,'data-result얌');
 	      var filenames = data.result.data;
-
+				// console.log(filenames,'filenames얌');
 	      for (var i = 0; i < filenames.length; i++) {
 	        var val = fiFilenames.val();
 	        if (val.length > 0) val += ",";
@@ -102,20 +130,24 @@
 				paginationType: 'fraction',
 				paginationClickable: true
 		});
-		
 
 
-
-		$(document).on('ready', function(e){
-
-			console.log(swiper.realIndex)
-			console.log($('.writezzal-page.swiper-slide-active').attr('data-no'))
+		$('.test-btn').on('click',function(e){
+			console.log(indexNum)
+			console.log($('.images-div'))
 		})
 
+
+
+var ssl=0;
     $(document).on('click', '.append-slide', function(e) {
     	e.preventDefault();
     	templateFn = Handlebars.compile($('#addpage-template').text())
     	swiper.appendSlide(templateFn())
-    })
+			var ssl = parseInt($('.swiper-slide').length-1)
+			console.log(ssl)
+		  $($('.swiper-slide')[ssl]).attr('data-no',(ssl+1))
+			// $('.swiper-slide')[ssl-1].attr('data-no',ssl-1)
+		})
 
 })(jQuery);
