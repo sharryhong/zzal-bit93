c34556dc594ac0de8044c12fbff85075403fedb5
정성룡 <fyd519@gmail.com>
2017-08-03 09:37:00 +0900

    input placeholder -> value
    .

--- src/main/webapp/js/collectupdate.js
+++ src/main/webapp/js/collectupdate.js
@@ -1,35 +1,6 @@
 (function($){
 	'use strict';
 
-/*let	maincollectdetail1 = $('#main-collectdetail1');
-let maincollectdetail2 = $('#main-collectdetail2');
-let maincollectdetail3 = $('#main-collectdetail3');
-
-
-$.getJSON('collectdetail.json',function(result){
-  console.log(result)
-
-  let templateFn1 = Handlebars.compile($('#collectdetail-template1').text())
-  let generatedHTML1 = templateFn1(result.data)
-  console.log(generatedHTML1)
-  maincollectdetail1.text('')
-  maincollectdetail1.html(generatedHTML1)
-
-	let templateFn2 = Handlebars.compile($('#collectdetail-template2').text())
-	let generatedHTML2 = templateFn2(result.data)
-	console.log(generatedHTML2)
-	maincollectdetail2.text('')
-	maincollectdetail2.html(generatedHTML2)
-	})
-$.getJSON('collectdetail.json',function(result){
-  console.log(result)
-
-  let templateFn3 = Handlebars.compile($('#collectdetail-template3').text())
-  let generatedHTML3 = templateFn3(result.data)
-  console.log(generatedHTML3)
-  maincollectdetail3.text('')
-  maincollectdetail3.html(generatedHTML3)
-	})*/
 	var no = 0
 	try {
 	  no = location.href.split('?')[1].split('=')[1]
@@ -44,8 +15,8 @@ $.getJSON('collectdetail.json',function(result){
 	})
 
 	$.getJSON('collect/detail.json', {'no': no}, function(result) {
-			$("#collect-add-title").attr("placeholder", result.data.title);
-			$('#collect-add-content').attr("placeholder", result.data.content);
+			$("#collect-add-title").attr("value", result.data.title);
+			$('#collect-add-content').attr("value", result.data.content);
 			$("#myonoffswitch").attr('checked', result.data.isPublic);
 			/*$('#collect-add-title').text(result.data.title)
 			$('#collect-add-content').text(result.data.content) 값은 들어오는데 input 태그에 입력이안됨*/
