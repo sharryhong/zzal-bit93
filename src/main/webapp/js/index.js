c527443715f9c95c32ce1a4693279dddcbddd81a
sharryhong <kshopzoa15@gmail.com>
2017-08-03 14:23:20 +0900

    슬라이더 수정

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -5,31 +5,49 @@ $(document).ready(function(){
 		generateHandlebars(result, $('#main-template'), $('#zzal-list'));
 	})
 	
+	// index.html 금주의 인기짤강 & 슬라이드에 뿌리기 
 	$.getJSON('zzal/zzalBestList.json', function(result) {
 		console.log(result.data)
-		generateHandlebars(result, $('#main-slide-template'), $('#main-slide'));
 		
 		var zzal = result.data.zzalList
-		var $target = $('#best-week-zzal')
-		var newEl = ''
+		var $target = $('#best-week-zzal'),
+		    $targetSlide = $('#main-slide')
+		var newEl = '',
+		    newSlideEl = ''
 		for (let i = 0; i < result.data.zzalList.length; i++) {
 			newEl += [
 				'<li data-zzno="'+zzal[i].zzno+'"><a href="#"><span class="ranking">'+(i+1)+'</span><span class="user-name"> ['+zzal[i].member.nick+'] </span><span class="zzal-title">'+zzal[i].title+'</span></a></li>'
 			].join('')
+			
+			newSlideEl += [
+				'<li class="swiper-slide"><img src="upload/'+zzal[i].mainPic+'"><p class="title">'+zzal[i].title+'</p><span class="rank">'+(i+1)+'</span></li>'
+			]
 		}
 		$target.html(newEl)
+		$targetSlide.html(newSlideEl)
 		
 		swiperFn()
 	})
 	
 	function swiperFn() {
-		$('.index-page .bxslider').bxSlider({
+		/*$('.index-page .bxslider').bxSlider({
 	    	auto: true,
 	    	mode: 'fade',
 	    	speed: 300,
 	    	captions: true,
 	    	pager: false
-		});
+		});*/
+		var swiper = new Swiper('.slider .swiper-container', {
+	        pagination: '.swiper-pagination',
+	        nextButton: '.swiper-button-next',
+	        prevButton: '.swiper-button-prev',
+	        paginationClickable: true,
+	        centeredSlides: true,
+	        autoplay: 2500,
+	        autoplayDisableOnInteraction: false,
+	        effect: 'fade',
+	        loop: true
+	    });
 	}
 	
 });
