3f868567abe0aaf4331e605372ab329d2ef8252a
SinYongJu <ygp007@naver.com>
2017-08-17 10:10:45 +0900

    ㅋㅋ

--- src/main/java/bitcamp/java93/domain/Search.java
+++ src/main/java/bitcamp/java93/domain/Search.java
@@ -2,20 +2,38 @@ package bitcamp.java93.domain;
 
 public class Search {
 	int szzalno;
+	int slcnt;
+	int shitcnt;
+	int sreplcnt;
 	String stitle;
 	String snick;
 	String scont;
 	String scategoryName;
-	@Override
-	public String toString() {
-		return "Search [szzalno=" + szzalno + ", stitle=" + stitle + ", snick=" + snick + ", scont=" + scont
-				+ ", scategoryName=" + scategoryName + "]";
-	}
+	String smpic;
+	String szmpic;
 	public int getSzzalno() {
 		return szzalno;
 	}
 	public void setSzzalno(int szzalno) {
 		this.szzalno = szzalno;
+	}
+	public int getSlcnt() {
+		return slcnt;
+	}
+	public void setSlcnt(int slcnt) {
+		this.slcnt = slcnt;
+	}
+	public int getShitcnt() {
+		return shitcnt;
+	}
+	public void setShitcnt(int shitcnt) {
+		this.shitcnt = shitcnt;
+	}
+	public int getSreplcnt() {
+		return sreplcnt;
+	}
+	public void setSreplcnt(int sreplcnt) {
+		this.sreplcnt = sreplcnt;
 	}
 	public String getStitle() {
 		return stitle;
@@ -41,6 +59,25 @@ public class Search {
 	public void setScategoryName(String scategoryName) {
 		this.scategoryName = scategoryName;
 	}
+	public String getSmpic() {
+		return smpic;
+	}
+	public void setSmpic(String smpic) {
+		this.smpic = smpic;
+	}
+	public String getSzmpic() {
+		return szmpic;
+	}
+	public void setSzmpic(String szmpic) {
+		this.szmpic = szmpic;
+	}
+	@Override
+	public String toString() {
+		return "Search [szzalno=" + szzalno + ", slcnt=" + slcnt + ", shitcnt=" + shitcnt + ", sreplcnt=" + sreplcnt
+				+ ", stitle=" + stitle + ", snick=" + snick + ", scont=" + scont + ", scategoryName=" + scategoryName
+				+ ", smpic=" + smpic + ", szmpic=" + szmpic + "]";
+	}
+	
 	
 	
     
--- src/main/webapp/js/search.js
+++ src/main/webapp/js/search.js
@@ -1,7 +1,7 @@
 (function($) {
 	'use strict';
 
-	var availableTags;
+	var availableTags=[];
 	var fisearchvalue;
 	//$(window).on("load", function() {
 
@@ -17,71 +17,98 @@
 		autocon()
 
 
-
+		e.preventDefault();
 	});
-
+	// var InitSearchPack = function (){
+	// 	this.setSpack=function(obj){
+	// 		this.zzalNo=obj.szzalno,
+	// 		this.nick=obj.snick,
+	// 		this.userPic=obj.smpic,
+	// 		this.mainPic=obj.szmpic,
+	// 		this.title=obj.stitle,
+	// 		this.hitcnt=obj.shitcnt,
+	// 		this.lcnt=obj.slcnt,
+	// 		this.replcnt=obj.sreplcnt,
+	// 		this.catgName=obj.scategoryName
+	// 	}
+	// }
 
 
 
 	// 검색후 엔터 키보드 눌렀을 때, 과거에 검색한 결과 리스트추가.
 	// ver search 기능의 적용!
 	$(".search-line").keydown(function(key) {
-//		$('#searchcon').attr('autocomplete', 'on');
 
-//		$(':input').live('focus',function(){
-//		$(this).attr('autocomplete', 'off');
-//		});
 
-//		fisearchvalue = $('#searchcon').val()  
-		
-			if (key.keyCode == 13) {
-				console.log($('#searchcon').val())    	      	  
-				if($('#searchcon').val() != ""){	
-//
-//					$.ajax({
-//						method:"GET",
-//						url:'/zzal-bit93/search/found.json',
-//						data:{'keyword': $('#searchcon').val()},
-//						success : function(data){console.log(data,"검색 객체임")
-//						}  			 
-//					})  
-					location.href='/zzal-bit93/searchresult.html'
-				}
+		if (key.keyCode == 13) {
+			console.log($('#searchcon').val())
+			if($('#searchcon').val() != ""){
+				// location.href='/zzal-bit93/searchresult.html'
 			}
+		}
 
 
 
 	});
-function autocon(){
-	$.ajax({
-		url:'/zzal-bit93/search/autosearch.json',
-		method:'POST',
-		success : function(data){console.log(data,"성공 객체임")
+	function autocon(){
+		$.ajax({
+			url:'/zzal-bit93/search/found.json',
+			method:'POST',
+			success : function(data){console.log(data,"성공 객체임")
 
-			availableTags=data.data.zzalList;
-		    console.log(availableTags)
+				availableTags=data.data.zzalList
 
-		$('#searchcon').autocomplete({	    		  
-			source:availableTags,
-			minLength:0,
-			appendTo : ".suggest-all-line",
-			select: function(event,ui){
+
+
+			console.log(availableTags[0])
+
+			$('#searchcon').autocomplete({
+				source:availableTags,
+				minLength:0,
+				appendTo : ".suggest-all-line",
+				select: function(event,ui){
+
+					$('#searchcon').val(ui.item.label)
+					return false;
+				}
+//			select: function(event, ui){
+//			console.log(ui)
+//			}
+			}).autocomplete("instance")._renderItem = function(ul, item){
+				console.log(item)
+				templateFn = Handlebars.compile($('#search-template').text())
+					//
+					// <ul>
+					// 	<li class='suggest-in-line'><a href='#' class='link-search'><div class="left-zzal"></div></a>
+					// 		<div class="search-mid-con">
+					// 			<span class="info user-info"><img class="user-pic" src="image/sul.jpg" /><span class="name">유끼만두냔</span></span>
+					// 			<span class="info cata-name">음식,먹방</span>
+					// 			<span class="info lect-titl">만두만두 맛나게 먹기!</span>
+					// 			<span class="info cdt">2017-08-16</span>
+					// 		</div>
+					// 		<div class="detail-info">
+					// 			<ul>
+					// 				<li><span class="left"><i class='fa fa-heart-o name' aria-hidden='true'></i></span><span class="right">121212</span></li>
+					// 				<li><span class="left"><i class='fa fa-commenting-o' aria-hidden='true'></i></span><span class="right">121212</span></li>
+					// 				<li><span class="left"><i class='fa fa-eye' aria-hidden='true'></i></span><span class="right">121212</span></li>
+					// 			</ul>
+					// 		</div>
+					// 	 <a href='#' class='btn-key'><i class='fa fa-times'aria-hidden='true'></i></a>
+					//  </li>
+				  // </ul>
+
+					/*"<li class = 'suggest-in-line'>"
+													+"<a href='#' class='link-search'></a>"
+													+"<a href='#' class='btn-key'><i class='fa fa-rocket' aria-hidden='true'></i></a>"
+								  +"</li>"*/
 				
-				$('#searchcon').val(ui.item.label)
-				return false;
+				return $(templateFn(item)).appendTo(ul);
 			}
-//		select: function(event, ui){
-//		console.log(ui)
-//		}
-		}).autocomplete("instance")._renderItem = function(ul, item){
-		    	return $("<li class = 'suggest-one-line'><a href='#' class='link-search'>"+item.value+"</a><a href='#' class='btn-key'><i class='fa fa-rocket' aria-hidden='true'></i></a></li>")
-		    			.appendTo(ul);
-		  }
-		
-		},
-		dataType: 'json'
-	})
-}//autocon
+
+			},
+			dataType: 'json'
+		})
+	}//autocon
 
 	// 검색헤더 상태에서 취소버튼 누르면 일반 헤더로 전환됨
 	$('.cancle-btn').click(function() {
